11f9166e0b793ce5a2e755a31efdc4100cd8a48f
Mahdi Lamb <mahdilamb@gmail.com>
2020-10-21 15:22:14 +0100

    make skinnable

--- src/main/java/qupath/lib/gui/Knob.java
+++ src/main/java/qupath/lib/gui/Knob.java
@@ -1,79 +1,78 @@
 package qupath.lib.gui;
 
-import javafx.beans.property.*;
-import javafx.geometry.Pos;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.scene.control.Control;
+import javafx.scene.control.Skin;
+import javafx.scene.control.SkinBase;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Stop;
-import javafx.scene.text.Font;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 import javafx.scene.text.Text;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A custom JavaFX knob control themed closely to the JavaFX's Moderna
  */
-public class Knob extends StackPane {
-    final Canvas canvas = new Canvas();
-    final GraphicsContext gc = canvas.getGraphicsContext2D();
-    private final double padding = 2;
+public class Knob extends Control {
+
+    private static final class KnobSkin extends SkinBase<Knob> {
+        protected KnobSkin(Knob control) {
+            super(control);
+        }
+    }
+
     private final DoubleProperty rotationProperty = new SimpleDoubleProperty(0);
-    private final double radius;
     private final BooleanProperty snapEnabled = new SimpleBooleanProperty(false);
-    private final BooleanProperty tickMarksVisible = new SimpleBooleanProperty(false);
-    private final BooleanProperty showAngle = new SimpleBooleanProperty(false);
     private final DoubleProperty tickSpacing = new SimpleDoubleProperty(10);
-    private final ObjectProperty<Font> textFont = new SimpleObjectProperty<>(new Text().getFont());
+    private final Circle outerCircle = new Circle();
+    private final Circle innerCircle = new Circle(5);
+    private final Path tickMarks = new Path();
     private final Text angle = new Text("0.0\u00B0");
-    private final double[] center;
-    private final List<double[]> snapMarks = new ArrayList<>();
 
-    public Knob(double diameter) {
-        getChildren().addAll(canvas, angle);
-        StackPane.setAlignment(canvas, Pos.CENTER);
-        StackPane.setAlignment(angle, Pos.CENTER);
+    public Knob() {
+        //add nodes
+        getChildren().addAll(outerCircle, tickMarks, innerCircle, angle);
+        //enable focus
         setFocusTraversable(true);
-        this.radius = Math.max(10, diameter * .5);
-        center = new double[]{padding + .5 * diameter, padding + .5 * diameter};
-        canvas.setWidth(diameter + padding + padding);
-        canvas.setHeight(canvas.getWidth());
-        disabledProperty().addListener((observable, oldValue, newValue) -> repaint());
-        focusedProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue && !isDisabled()) {
-                //just adding a thick line - can draw over the current control
-                gc.setStroke(Color.web("#039ED3"));
-                gc.setLineWidth(2);
-                gc.strokeOval(padding, padding, diameter, diameter);
-            } else {
-                //force repaint
-                repaint();
-            }
-        });
+        //make sure circles are
+        innerCircle.setManaged(false);
+        outerCircle.setManaged(false);
+        //add CSS
+        getStylesheets().add(getClass().getResource("/css/knob.css").toExternalForm());
+        // set style class
+        getStyleClass().setAll("knob");
+        outerCircle.getStyleClass().setAll("knob-outer");
+        innerCircle.getStyleClass().setAll("knob-inner");
+        angle.getStyleClass().setAll("knob-text");
+        tickMarks.getStyleClass().setAll("knob-track-mark");
+        //prevent mouse events being trapped by some nodes
+        innerCircle.setMouseTransparent(true);
+        angle.setMouseTransparent(true);
 
-        addEventHandler(MouseEvent.MOUSE_DRAGGED, e -> updateRotationWithMouseEvent(e));
+        tickMarks.setVisible(false);
+        //add event handlers
+        outerCircle.addEventHandler(MouseEvent.MOUSE_DRAGGED, e -> updateRotationWithMouseEvent(e));
         addEventHandler(KeyEvent.KEY_PRESSED, e -> {
             switch (e.getCode()) {
-                case UP:
-                    rotationProperty().set(rotationProperty().get() - (isSnapToTicks() ? tickSpacing.get() : 1));
+                case LEFT:
+                    rotationProperty().set(rotationProperty().get() - (isSnapToTicks() ? getTickSpacing() : 1));
                     break;
-                case DOWN:
-                    rotationProperty().set(rotationProperty().get() + (isSnapToTicks() ? tickSpacing.get() : 1));
+                case RIGHT:
+                    rotationProperty().set(rotationProperty().get() + (isSnapToTicks() ? getTickSpacing() : 1));
                     break;
 
             }
         });
-        addEventHandler(ScrollEvent.ANY, e -> rotationProperty().set(rotationProperty().get() + (e.isShiftDown() ? e.getDeltaX() : e.getDeltaY()) * (isSnapToTicks() ? tickSpacing.get() : 1)));
-        addEventHandler(MouseEvent.MOUSE_PRESSED, e -> {
-            final double[] vector = new double[]{e.getX() - center[0] - canvas.getLayoutX(), e.getY() - center[1] - canvas.getLayoutY()};
-            if (!focusedProperty().get() && dot(vector, vector) <= radius * radius) {
+        outerCircle.addEventHandler(ScrollEvent.ANY, e -> rotationProperty().set(rotationProperty().get() + (e.isShiftDown() ? e.getDeltaX() : e.getDeltaY()) * (isSnapToTicks() ? getTickSpacing() : 1)));
+        outerCircle.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> {
+            final double[] vector = new double[]{e.getX() - outerCircle.getCenterX(), e.getY() - outerCircle.getCenterY()};
+            if (!focusedProperty().get() && dot(vector, vector) <= outerCircle.getRadius() * outerCircle.getRadius()) {
                 requestFocus();
             }
         });
@@ -84,31 +83,28 @@ public class Knob extends StackPane {
             }
             updateSnapMarks();
         });
-        snapEnabled.addListener((observable, oldValue, newValue) -> repaint());
-        tickMarksVisible.addListener((observable, oldValue, newValue) -> repaint());
-        parentProperty().addListener((observable, oldValue, newValue) -> repaint());
-        styleProperty().addListener((observable, oldValue, newValue) -> repaint());
-        showAngle.addListener((observable, oldValue, newValue) -> angle.setVisible(newValue));
-        textFont.addListener((observable, oldValue, newValue) -> angle.setFont(newValue));
-        updateSnapMarks();
-    }
-
-    public Knob() {
-        this(100);
 
     }
 
-    //keep a list of snap mark vectors so they don't need to be recalculated; Always calls repaint
+
     private void updateSnapMarks() {
-        snapMarks.clear();
+        tickMarks.getElements().clear();
+
         final double radians = Math.toRadians(tickSpacing.get());
         for (double i = 0; i < Math.PI * 2; i += radians) {
-            snapMarks.add(new double[]{
-                    Math.sin(i), -Math.cos(i)
-            });
-        }
+            final double x = Math.sin(i);
+            final double y = -Math.cos(i);
 
-        repaint();
+            final double x0 = outerCircle.getCenterX() + (outerCircle.getRadius() - innerCircle.getRadius() - 2) * x;
+            final double y0 = outerCircle.getCenterY() + (outerCircle.getRadius() - innerCircle.getRadius() - 2) * y;
+            final double x1 = outerCircle.getCenterX() + (outerCircle.getRadius() - innerCircle.getRadius() - innerCircle.getRadius()) * x;
+            final double y1 = outerCircle.getCenterY() + (outerCircle.getRadius() - innerCircle.getRadius() - innerCircle.getRadius()) * y;
+            tickMarks.getElements().addAll(
+                    new MoveTo(x0, y0),
+                    new LineTo(x1, y1)
+            );
+
+        }
     }
 
     /**
@@ -118,7 +114,7 @@ public class Knob extends StackPane {
         if (isDisabled()) {
             return;
         }
-        final double[] currentVector = normalize(e.getX() - center[0] - canvas.getLayoutX(), e.getY() - center[1] - canvas.getLayoutY());
+        final double[] currentVector = normalize(e.getX() - outerCircle.getCenterX() - getLayoutX(), e.getY() - outerCircle.getCenterY() - getLayoutY());
         final double angle = Math.atan2(currentVector[0], -currentVector[1]);
         rotationProperty.set(Math.toDegrees(angle >= 0 ? angle : Math.PI + Math.PI + angle));// atan2 of dot product and determinant of current vector verses up (0,-1). As x of up vector is 0, can simplify
     }
@@ -140,15 +136,25 @@ public class Knob extends StackPane {
             if (getValue() < halfSnap || getValue() > 360 - halfSnap) {
                 rotationProperty.set(0);
             } else {
-
                 rotationProperty.set(tickSpacing.get() * (Math.round(getValue() / tickSpacing.get())));
             }
             rotationProperty.set(tickSpacing.get() * (Math.round(getValue() / tickSpacing.get())));
 
         }
+        updateTextAndInnerCircle();
+    }
 
-        repaint();
+    private void updateTextAndInnerCircle() {
         angle.setText(String.format("%.1f\u00B0", getValue()));
+        final double radians = Math.toRadians(rotationProperty.get());
+        final double[] currentVector = new double[]{
+                Math.sin(radians),
+                -Math.cos(radians)
+        };
+        final double x = outerCircle.getCenterX() + (outerCircle.getRadius() - innerCircle.getRadius() - 5) * currentVector[0];
+        final double y = outerCircle.getCenterY() + (outerCircle.getRadius() - innerCircle.getRadius() - 5) * currentVector[1];
+        innerCircle.setCenterX(x);
+        innerCircle.setCenterY(y);
     }
 
     /**
@@ -207,6 +213,11 @@ public class Knob extends StackPane {
         return rotationProperty.get();
     }
 
+    /**
+     * set rotation value
+     *
+     * @param rotation value
+     */
     public void setValue(double rotation) {
         this.rotationProperty.set(rotation);
     }
@@ -242,7 +253,7 @@ public class Knob extends StackPane {
      * @param visible whether to show tick marks
      */
     public void setShowTickMarks(boolean visible) {
-        tickMarksVisible.set(visible);
+        tickMarks.setVisible(visible);
     }
 
     /**
@@ -251,60 +262,33 @@ public class Knob extends StackPane {
      * @param visible whether to display the angle
      */
     public void setShowValue(boolean visible) {
-        showAngle.set(visible);
+        angle.setVisible(visible);
     }
 
-    public void setValueFont(Font font) {
-        this.textFont.set(font);
+    /**
+     * get the spacing between ticks
+     *
+     * @return the spacing ticks
+     */
+    public double getTickSpacing() {
+        return tickSpacing.get();
     }
 
-    protected void repaint() {
-        final double opacity = isDisabled() ? 0.4 : 1;
-        gc.clearRect(0, 0, getWidth(), getHeight());
-        if (getScene() != null) {
-            System.out.println(this.lookup(".root"));
+    @Override
+    protected Skin<?> createDefaultSkin() {
+        return new KnobSkin(this);
+    }
 
-        }
-        gc.setFill(new LinearGradient(0, 0, 0, 1, true, CycleMethod.NO_CYCLE, new Stop(1, Color.grayRgb(207, opacity)),
-                new Stop(0, Color.grayRgb(237, opacity))));
-        final double diameter = 2 * radius;
-        double positionIndicatorRadius = 5;
-        final double positionIndicatorDiameter = 2 * positionIndicatorRadius;
-        gc.fillOval(padding, padding, diameter, diameter);
-        if (isFocused() && !isDisabled()) {
-            gc.setStroke(Color.web("#039ED3", opacity));
-            gc.setLineWidth(2);
-        } else {
-            gc.setStroke(Color.grayRgb(208, opacity));
-            gc.setLineWidth(1);
-
-        }
-
-        gc.strokeOval(padding, padding, diameter, diameter);
-        final double radians = Math.toRadians(rotationProperty.get());
-        final double[] currentVector = new double[]{
-                Math.sin(radians),
-                -Math.cos(radians)
-        };
-        final double x = center[0] + (radius - positionIndicatorRadius - 5) * currentVector[0];
-        final double y = center[1] + (radius - positionIndicatorRadius - 5) * currentVector[1];
-        gc.setLineWidth(1);
-        if (tickMarksVisible.get()) {
-            gc.setStroke(Color.grayRgb(153, opacity));
-
-            for (final double[] mark : snapMarks) {
-                final double x0 = center[0] + (radius - positionIndicatorRadius - 2) * mark[0];
-                final double y0 = center[1] + (radius - positionIndicatorRadius - 2) * mark[1];
-                final double x1 = center[0] + (radius - positionIndicatorDiameter) * mark[0];
-                final double y1 = center[1] + (radius - positionIndicatorDiameter) * mark[1];
-                gc.strokeLine(x0, y0, x1, y1);
-
-            }
-        }
-
-        gc.setFill(Color.grayRgb(102, opacity));
-
-        gc.fillOval((x - positionIndicatorRadius), (y - positionIndicatorRadius), (positionIndicatorRadius * 2), (positionIndicatorRadius * 2));
+    @Override
+    protected void layoutChildren() {
+        super.layoutChildren();
+        outerCircle.setRadius((Math.min(getWidth(), getHeight()) * .5) - 5);
+        outerCircle.setCenterX(getWidth() * .5);
+        outerCircle.setCenterY(getHeight() * .5);
+        innerCircle.setRadius(outerCircle.getRadius() * .05);
+        updateTextAndInnerCircle();
+        updateSnapMarks();
 
     }
+
 }
--- src/main/java/qupath/lib/gui/Test.java
+++ src/main/java/qupath/lib/gui/Test.java
@@ -1,28 +1,22 @@
 package qupath.lib.gui;
 
 import javafx.application.Application;
-import javafx.beans.property.BooleanProperty;
-import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Slider;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import java.net.URISyntaxException;
 
 public class Test extends Application {
     @Override
-    public void start(Stage stage) throws URISyntaxException {
+    public void start(Stage stage) {
         final Knob knob = new Knob();
         final BorderPane root = new BorderPane();
         root.setCenter(knob);
+        root.setTop(new Slider());
         knob.rotationProperty().addListener((observable, oldValue, newValue) -> System.out.println(newValue.doubleValue()));
         knob.setTickSpacing(30);
-        knob.setValueFont(new Font(16));
         knob.setShowValue(true);
         knob.setSnapToTicks(false);
         knob.setOnKeyPressed(e -> {
@@ -40,7 +34,7 @@ public class Test extends Application {
             }
         });
 
-        final Scene scene = new Scene(root,500,400);
+        final Scene scene = new Scene(root, 500, 400);
         stage.setScene(scene);
         scene.getStylesheets().add(getClass().getResource("/css/dark.css").toExternalForm());
 
